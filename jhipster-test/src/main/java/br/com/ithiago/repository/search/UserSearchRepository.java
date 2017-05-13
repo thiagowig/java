8784e1b6c477a6b27ac08783f917c1bedcd193f4
Thiago FOnseca <dev.thiago@gmail.com>
2017-05-13 16:32:09 -0300

    Including jhipster

---
+++ jhipster-test/src/main/java/br/com/ithiago/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package br.com.ithiago.repository.search;
+
+import br.com.ithiago.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
