7e539194774f586ce63b68f1b4c73f474639362f
Thiago Fonseca <dev.thiago@gmail.com>
2017-09-23 15:43:51 -0300

    Resolving problem

--- kotlin/blog/src/main/kotlin/br/com/ithiago/blog/BlogApplication.kt
+++ kotlin/blog/src/main/kotlin/br/com/ithiago/blog/BlogApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class BlogApplication
+open class BlogApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(BlogApplication::class.java, *args)
