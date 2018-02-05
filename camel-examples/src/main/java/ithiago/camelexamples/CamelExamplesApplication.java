6b8d84f6414dc156a255d8603916b38841dbd411
Thiago Fonseca <dev.thiago@gmail.com>
2018-02-05 17:39:27 -0200

    Initial commit

---
+++ camel-examples/src/main/java/ithiago/camelexamples/CamelExamplesApplication.java
@@ -0,0 +1,12 @@
+package ithiago.camelexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelExamplesApplication.class, args);
+	}
+}
