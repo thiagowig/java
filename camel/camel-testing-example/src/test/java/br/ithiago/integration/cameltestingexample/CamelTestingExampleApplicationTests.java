eedb7a5759cfb98340156447cb6437b4c053caab
gotosfo <dev.thiago@gmail.com>
2018-11-09 17:05:17 -0200

    Incluind the camel test

---
+++ camel/camel-testing-example/src/test/java/br/ithiago/integration/cameltestingexample/CamelTestingExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ithiago.integration.cameltestingexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelTestingExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ spring-boot/spring-cloud-contract/src/main/java/com/ithiago/test/springcloudcontract/SpringCloudContractApplication.java
@@ -0,0 +1,12 @@
+package com.ithiago.test.springcloudcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractApplication.class, args);
+	}
+}
