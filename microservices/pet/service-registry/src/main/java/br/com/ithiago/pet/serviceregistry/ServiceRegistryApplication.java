02a75cc13e4794b2a7380f79c2135aba9309ec8a
Thiago Fonseca <dev.thiago@gmail.com>
2018-12-06 20:08:51 -0200

    Creating the service registry

---
+++ microservices/pet/service-registry/src/main/java/br/com/ithiago/pet/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,12 @@
+package br.com.ithiago.pet.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
