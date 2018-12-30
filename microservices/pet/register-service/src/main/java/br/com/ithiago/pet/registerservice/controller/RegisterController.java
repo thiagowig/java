3782a1ea0dca16e86322d765e352d4117d9f4652
Thiago Fonseca <dev.thiago@gmail.com>
2018-12-30 13:37:57 -0200

    Including load balancer and hystrix

--- microservices/pet/register-service/src/main/java/br/com/ithiago/pet/registerservice/controller/RegisterController.java
+++ microservices/pet/register-service/src/main/java/br/com/ithiago/pet/registerservice/controller/RegisterController.java
@@ -3,7 +3,9 @@ package br.com.ithiago.pet.registerservice.controller;
 import br.com.ithiago.pet.registerservice.domain.RegisterRequest;
 import br.com.ithiago.pet.registerservice.domain.RegisterResponse;
 import br.com.ithiago.pet.registerservice.service.RegisterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,13 @@ import java.util.List;
  * Created by thiago on 12/1/18.
  */
 @RestController
+@Slf4j
 public class RegisterController {
 
     private RegisterService service;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @Autowired
     public RegisterController(RegisterService service) {
@@ -25,6 +31,8 @@ public class RegisterController {
 
     @PostMapping(value = "/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        log.info("THE SERVER PORT: " + serverPort);
+
         RegisterResponse response = service.registerUser(request);
 
         ResponseEntity<RegisterResponse> responseEntity = new ResponseEntity(response, HttpStatus.OK);
--- microservices/pet/service-registry/src/main/java/br/com/ithiago/pet/serviceregistry/ServiceRegistryApplication.java
+++ microservices/pet/service-registry/src/main/java/br/com/ithiago/pet/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package br.com.ithiago.pet.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
