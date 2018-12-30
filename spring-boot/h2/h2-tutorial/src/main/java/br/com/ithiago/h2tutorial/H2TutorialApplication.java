a823171cc0f62f92fdd781af76729052c9545b24
Thiago Fonseca <dev.thiago@gmail.com>
2018-12-30 20:06:50 -0200

    [H2 testing - Creating H2 project]

---
+++ spring-boot/h2/h2-tutorial/src/main/java/br/com/ithiago/h2tutorial/H2TutorialApplication.java
@@ -0,0 +1,14 @@
+package br.com.ithiago.h2tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2TutorialApplication.class, args);
+	}
+
+}
+
