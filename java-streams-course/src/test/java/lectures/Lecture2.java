1c87538f45f5d3f28cd08c80d6458cfeb414531f
Thiago Santos Fonseca <dev.thiago@gmail.com>
2019-06-25 15:23:40 -0300

    Including lectures

--- java-streams-course/src/test/java/lectures/Lecture2.java
+++ java-streams-course/src/test/java/lectures/Lecture2.java
@@ -10,20 +10,28 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
+    IntStream.range(0, 10)
+            .forEach(System.out::println);
 
+    IntStream.rangeClosed(0, 10)
+            .forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
 
+    IntStream.range(0, people.size())
+            .forEach(index -> System.out.println(people.get(index)));
+
+    people.forEach(System.out::println);
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1)
+            .limit(20)
+            .filter(number -> number % 2 == 0)
+            .forEach(System.out::println);
   }
 }
