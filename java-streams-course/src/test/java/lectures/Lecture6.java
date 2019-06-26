55bef6be082fe5f5b6591949d5f008ba0f7685cd
Thiago Fonseca <dev.thiago@gmail.com>
2019-06-25 22:42:43 -0300

    Implementing the lecture 6

--- java-streams-course/src/test/java/lectures/Lecture6.java
+++ java-streams-course/src/test/java/lectures/Lecture6.java
@@ -14,12 +14,22 @@ public class Lecture6 {
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    Integer integer = Arrays.stream(numbers)
+            .findAny()
+            .get();
+
+    System.out.println(integer);
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    Integer integer = Arrays.stream(numbers)
+            .findFirst()
+            .get();
+
+    System.out.println(integer);
   }
 }
 
