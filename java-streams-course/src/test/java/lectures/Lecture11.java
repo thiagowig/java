2ee75c94065c1c4040acaa8789c775021fa45cb6
Thiago Fonseca <dev.thiago@gmail.com>
2019-06-26 23:21:14 -0300

    Implementing the lecture 10

--- java-streams-course/src/test/java/lectures/Lecture11.java
+++ java-streams-course/src/test/java/lectures/Lecture11.java
@@ -10,13 +10,23 @@ public class Lecture11 {
 
   @Test
   public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+      List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+      StringBuilder text = new StringBuilder();
 
+      names.forEach(name -> text.append(name + ", "));
+
+      System.out.println(text.substring(0, text.length() -2));
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+      List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+
+      String collect = names.stream()
+              .map(String::toUpperCase)
+              .collect(Collectors.joining(" | "));
+
+      System.out.println(collect);
 
   }
 }
