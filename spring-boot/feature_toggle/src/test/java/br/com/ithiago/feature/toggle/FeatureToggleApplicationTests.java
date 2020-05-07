6f8e96fe3c92ca9b65c0690834b918a855cb067d
Thiago Fonseca <fonsecat@google.com>
2020-05-06 21:15:44 -0300

    Creating structure

---
+++ spring-boot/feature_toggle/src/test/java/br/com/ithiago/feature/toggle/FeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ithiago.feature.toggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
