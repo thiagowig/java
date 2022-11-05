f5d174ce5c26f2b172713c995ded2a6a1a6e7167
Thiago Fonseca <dev.thiago@gmail.com>
2022-11-04 22:13:31 -0300

    Adding more tests

---
+++ communication/web-socket/src/main/java/dev/thiagofonseca/websocket/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package dev.thiagofonseca.websocket.controller;
+
+import dev.thiagofonseca.websocket.model.Greeting;
+import dev.thiagofonseca.websocket.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
---
+++ security/mtls-demo/src/test/java/dev/thiagofonseca/security/mtls/MtlsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagofonseca.security.mtls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
